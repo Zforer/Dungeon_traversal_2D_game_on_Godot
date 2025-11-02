2f7ce3957d200115a33b68f04eced5ef9a3cd00d
Zforer <you@example.com>
2025-11-02 14:00:02 +0500

    added_code_and_sound_for_the_attack

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,10 +4,18 @@ extends CharacterBody2D
 const MAX_SPEED = 125
 const JUMP_VELOCITY = -450
 const GRAVITY = 1000
+const SLAM_FALL_SPEED = 800
+
+var is_slamming: bool = false
+var was_in_air: bool = false
 
 
 @onready var _animated_sprite = $AnimatedSprite2D
 @onready var sfx_jamp = $SFX_JAMP
+
+
+func _ready():
+	$SlimeHitbox.monitoring = false
 
 
 func _physics_process(delta):
@@ -19,14 +27,32 @@ func _physics_process(delta):
 	else:
 		velocity.y = 0
 
-	# Обработка прыжка
 	if Input.is_action_just_pressed("move_ap") and is_on_floor():
 		sfx_jamp.stop()
 		velocity.y = JUMP_VELOCITY
+		was_in_air = false
 		sfx_jamp.play()
 	
+	if Input.is_action_pressed("move_down") and not is_on_floor():
+		velocity.y = SLAM_FALL_SPEED
+		is_slamming = true
+		was_in_air = true
+
+	
+	if was_in_air and is_on_floor():
+		execute_slam_attack()
+		was_in_air = false
+		is_slamming = false
 	
 	move_and_slide()
+
+
+func execute_slam_attack():
+	#$Sprite2D.play("Atak")
+	$SFX_ATAC.play()
+	$SlimeHitbox.monitoring = true
+	await get_tree().create_timer(0.1).timeout
+	$SlimeHitbox.monitoring = false
 
 
 func _process(_delta):
